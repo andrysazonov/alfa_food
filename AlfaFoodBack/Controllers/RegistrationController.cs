67864f1a9e0b225dfbc19ab086075b4f4dd4dd24
elizShtol <eliz.shtol@gmail.com>
2021-04-05 12:27:47 +0500

    Registration phys

---
+++ AlfaFoodBack/Controllers/RegistrationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using AlfaFoodBack.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlfaFoodBack.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegistrationController : Controller
+    {
+        [HttpPost("phys")]
+        public IActionResult RegisterPhys(string login, string password, string name, string surname,
+            string phoneNumber = null)
+        {
+            User user;
+            try
+            {
+                user = new User(login, password, name, surname, phoneNumber);
+            }
+            catch (Exception e)
+            {
+                return new BadRequestResult();
+            }
+
+            using (var dbCon = DBUtils.GetDBConnection())
+            {
+                return user.Insert(dbCon);
+            }
+        }
+        
+    }
+}
--- AlfaFoodBack/Startup.cs
+++ AlfaFoodBack/Startup.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,10 +22,7 @@ namespace AlfaFoodBack
             services.AddControllersWithViews();
 
             // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/build";
-            });
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,18 +48,15 @@ namespace AlfaFoodBack
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}");
+                    "default",
+                    "{controller}/{action=Index}/{id?}");
             });
 
             app.UseSpa(spa =>
             {
                 spa.Options.SourcePath = "ClientApp";
 
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
+                if (env.IsDevelopment()) spa.UseReactDevelopmentServer("start");
             });
         }
     }
