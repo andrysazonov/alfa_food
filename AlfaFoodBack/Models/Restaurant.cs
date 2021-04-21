f61ef23932c92f8b2d88454e6c3edc2fb7875f2f
dedvnutree <tkachuk-p@mail.ru>
2021-04-21 16:44:41 +0500

    Написал скелет модели Ресторан, добавил комментарии

--- AlfaFoodBack/Models/Restaurant.cs
+++ AlfaFoodBack/Models/Restaurant.cs
@@ -10,38 +10,41 @@ namespace AlfaFoodBack.Models
     {
         private int currentOrderId;
         private int id;
-        private readonly string login;
+        private int ownerId; 
         private readonly string name;
         private Stack<Order> ordersHistory;
-        private readonly string password;
         private readonly string phoneNumber;
+        private readonly string address; //Надо определиться с форматом хранения адреса, а так же организовать проверку на валидность адреса
+        private readonly string description; //надо добавить возможность изменять описание ресторана и возможность не добавлять его при регистрации
+        private List<int> moderators; //список людей, имеющих право изменять данные о ресторане через приложения (прим. владелец и менеджер ресторана)
+        private List<int> workers; // id людей, работающих в ресторане, имеющих свои задачи в приложении (прим. официант, хостес)
 
-        public Restaurant(string login, string password, string name, string phoneNumber = null)
+        public Restaurant(int ownerId, string name, string address, string description, string phoneNumber = null)
         {
-            if (!IsLoginValid(login)) // Нужно вынести проверку логина, пароля и номера для ресторана и юзера в однельный класс чекер
-                throw new Exception("Invalid login");
-            if (!IsPasswordValid(password))
-                throw new Exception("Invalid password");
             if (!IsPhoneNumberValid(phoneNumber))
-                throw new Exception("Invalid phone number");
-            this.login = login;
-            this.password = password;
+                throw new Exception("Invalid phone number"); 
+            if (!IsAddressValid(phoneNumber))
+                throw new Exception("Invalid address");
+            this.ownerId = ownerId;
             this.name = name;
             this.phoneNumber = phoneNumber;
+            this.address = address;
+            this.description = description;
+            moderators = new List<int> { ownerId };
+            workers = new List<int>();
+
         }
 
         public IActionResult Insert(MySqlConnection dbCon)
         {
-            if (UserWithLoginExists(dbCon, login))
-                return new BadRequestResult();
             var command = dbCon.CreateCommand();
             command.CommandType = CommandType.Text;
             if (phoneNumber != null)
                 command.CommandText =
-                    $"INSERT INTO User (login, password, name, surname) VALUES({login}, {password}, {name}, {phoneNumber})";
+                    $"INSERT INTO Restaurant (ownerId, name, phoneNumber, address, description) VALUES({ownerId}, {name}, {phoneNumber}, {address}, {description})";
             else
                 command.CommandText =
-                    $"INSERT INTO User (login, password, name, surname) VALUES({login}, {password}, {name})";
+                    $"INSERT INTO Restaurant (ownerId, name, address, description) VALUES({ownerId}, {name}, {address}, {description})";
             try
             {
                 command.ExecuteNonQuery();
@@ -61,30 +64,78 @@ namespace AlfaFoodBack.Models
 
         public IDbEntity GetById(MySqlConnection dbCon, int id)
         {
+
+            var command = dbCon.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = $"SELECT name, phoneNumber, address, description" +
+                                    $"FROM Restaurant" +
+                                    $"WHERE id = {id}";
+
+            try
+            {
+                var reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                else
+                {
+                    throw new Exception("No rows found.");
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                throw new NotImplementedException();
+            }
+
             throw new NotImplementedException();
         }
 
-        public bool UserWithLoginExists(MySqlConnection dbCon, string login)
+        public IDbEntity GetRestaurantsByOwnerId(MySqlConnection dbCon, int id)
         {
+
             var command = dbCon.CreateCommand();
             command.CommandType = CommandType.Text;
-            command.CommandText = $"select * from user where login={login}";
-            return command.ExecuteNonQuery() > 0;
+            command.CommandText = $"SELECT name, phoneNumber, address, description" +
+                                    $"FROM Restaurant" +
+                                    $"WHERE ownerId = {id}";
+
+            try
+            {
+                var reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        throw new NotImplementedException();
+                    }
+                }
+                else
+                {
+                    throw new Exception("No rows found.");
+                }
+                reader.Close();
+            }
+            catch (Exception e)
+            {
+                throw new NotImplementedException();
+            }
+
+            throw new NotImplementedException();
         }
 
         private static bool IsPhoneNumberValid(string phoneNumber)
         {
-            return true;
+            throw new NotImplementedException();
         }
 
-        private static bool IsLoginValid(string login)
+        private static bool IsAddressValid(string phoneNumber)
         {
-            return true;
-        }
-
-        private static bool IsPasswordValid(string password)
-        {
-            return password.Length >= 6;
+            throw new NotImplementedException();
         }
     }
 }
