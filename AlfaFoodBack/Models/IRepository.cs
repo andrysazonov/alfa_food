432d4cba147f8c3485d4496923aa923d4fdc5e16
elizShtol <eliz.shtol@gmail.com>
2021-04-28 21:25:24 +0500

    IRepository

---
+++ AlfaFoodBack/Models/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+
+namespace AlfaFoodBack.Models
+{
+    public interface IRepository
+    {
+        public void Insert(MySqlConnection dbCon, IDbEntity entity);
+        public HttpResponseMessage Update(MySqlConnection dbCon, IDbEntity entity);
+        public HttpResponseMessage GetById(MySqlConnection dbCon, int id);
+    }
+}
--- AlfaFoodBack/Models/Order.cs
+++ AlfaFoodBack/Models/Order.cs
@@ -1,24 +1,24 @@
-﻿using System;
-using Microsoft.AspNetCore.Mvc;
-using MySql.Data.MySqlClient;
-
-namespace AlfaFoodBack.Models
-{
-    public class Order : IDbEntity
-    {
-        public IActionResult Insert(MySqlConnection dbCon)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IActionResult Update(MySqlConnection dbCon)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IDbEntity GetById(MySqlConnection dbCon, int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+﻿// using System;
+// using Microsoft.AspNetCore.Mvc;
+// using MySql.Data.MySqlClient;
+//
+// namespace AlfaFoodBack.Models
+// {
+//     public class Order : IRepository
+//     {
+//         public IActionResult Insert(MySqlConnection dbCon)
+//         {
+//             throw new NotImplementedException();
+//         }
+//
+//         public IActionResult Update(MySqlConnection dbCon)
+//         {
+//             throw new NotImplementedException();
+//         }
+//
+//         public IRepository GetById(MySqlConnection dbCon, int id)
+//         {
+//             throw new NotImplementedException();
+//         }
+//     }
+// }
