88a691393e1303b61cb702c1e7465ca019d87c3a
Denis <k1selevde@yandex.ru>
2021-05-24 22:06:03 +0500

    Guest page, same sections

--- AlfaFoodBack/ClientApp/src/pages/Guest/sections/AboutMobile/index.tsx
+++ AlfaFoodBack/ClientApp/src/pages/Guest/sections/AboutMobile/index.tsx
@@ -2,8 +2,20 @@ import React from "react"
 
 
 import alfaMobileGif from "../../../../assets/gifs/alfa-mobile.gif"
+import iphoneTemplate from  "../../../../assets/images/iphonee.png"
+//@ts-ignore
+// import { ReactComponent as IphoneTemplate } from "../../../../assets/svg/iphone_template.svg"
 
 import "./index.scss"
+
+const features = [
+    "Профиль с настройками аккаунта",
+    "Поиск по ресторанам и кафе",
+    "Рекомендации",
+    "Возсожность добавлять рестораны в избранное",
+    "Бронирование столика и предзаказ",
+    "Раздельная оплата"
+]
 
 const AboutMobile = () => {
     return (
@@ -11,16 +23,30 @@ const AboutMobile = () => {
             <div className="content">
                 <h3 className="aboutMobileSection__title">Мобильное приложение для физ.лиц</h3>
                 <div className="aboutMobileSection__content">
-                    <div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus, similique!</p>
-                    </div>
-                    <div className="aboutMobileSection__img-wrap">
+                    <ul className="aboutMobileSection__features">
+                        {
+                            features.map((feauture) => (
+                                <li>
+                                    <div
+                                        key={feauture}
+                                        className="aboutMobileSection__feature"
+                                    >
+                                        {feauture}
+                                    </div>
+                                </li>
+
+                            ))
+                        }
+                    </ul>
+                    <div className="aboutMobileSection__img-wrap" >
                         <img
                             src={alfaMobileGif}
                             alt="alfa-mobile-gif"
-                            height={"550px"}
-
+                            height={"525px"}
+                            width={"250px"}
                         />
+
+
                     </div>
                 </div>
 
--- AlfaFoodBack/ClientApp/src/pages/Guest/sections/Capabilities/index.tsx
+++ AlfaFoodBack/ClientApp/src/pages/Guest/sections/Capabilities/index.tsx
@@ -1,24 +1,62 @@
 import React from "react"
 
+import "./index.scss"
 
+
+const capibilities = [
+    {
+        title: "Статистика",
+        descr: "descr22",
+        image: ""
+    },
+    {
+        title: "База гостей",
+        descr: "descr22",
+        image: ""
+    },
+    {
+        title: "СМС рассылка для гостей",
+        descr: "descr22",
+        image: ""
+    },
+]
 
 const Capabilities = () => {
     return (
-        <div>
+        <section className="capabilitiesSection__section">
             <div className="content">
-                <div>
-                    <h5>Статистика</h5>
+                <div
+                    className="capabilitiesSection__content"
+                >
+                    {
+                        capibilities.map((cap, i) => (
+                            <div className={`capabilitiesSection__subsection ${i % 2 == 1 ? "subsection--order" : ""}`}>
+                                <div
+                                    className={`capabilitiesSection__subsection-image ${i % 2 == 1 ? "subsection-image--order" : "" }`}
+                                >
+                                    <img src={cap.image} alt=""/>
+                                </div>
+                                <div
+                                    className="capabilitiesSection__subsection-text"
+                                >
+                                    <h5
+                                        className="capabilitiesSection__subsection-title"
+                                    >{cap.title}</h5>
+                                    <p
+                                        className="capabilitiesSection__subsection-descr"
+                                    >
+                                        {cap.descr}
+                                    </p>
+                                </div>
 
+                            </div>
+                        ))
+                    }
                 </div>
-                <div>
-                    <h5>База гостей</h5>
-                </div>
-                <div>
-                    <h5>СМС рассылка для гостей</h5>
-                </div>
+
             </div>
 
-        </div>
+        </section>
     )
 }
 
